6358e308facc13244f92b9d713aca5f6c75fd7b8
Dylan Musil <dmusil@degreed.com>
2021-04-04 21:21:24 -0400

    Add nav. Move about me to component.

--- dmmusil/src/app/app.module.ts
+++ dmmusil/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
