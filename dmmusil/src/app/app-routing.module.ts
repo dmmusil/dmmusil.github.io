d4f47e752899125c8a70131f5ae12d205819b12c
Dylan Musil <dylanmusil@gmail.com>
2023-04-22 10:09:40 -0400

    Make about the default page

--- dmmusil/src/app/app-routing.module.ts
+++ dmmusil/src/app/app-routing.module.ts
@@ -1,33 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { MainComponent } from './main/main.component';
 import { AngularOnGithubPagesComponent } from './posts/angular-on-github-pages/angular-on-github-pages.component';
 import { AzureSqlServerlessComponent } from './posts/azure-sql-serverless/azure-sql-serverless.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: MainComponent,
-    title: 'Dylan Musil'
+  {
+    path: '',
+    component: AboutComponent,
+    title: 'Dylan Musil',
   },
   {
     path: 'about',
     component: AboutComponent,
-    title: 'Dylan Musil'
+    title: 'Dylan Musil',
   },
   {
     path: 'posts',
+    component: MainComponent,
     children: [
-      { 
-        path: 'azure-sql-serverless', 
+      {
+        path: 'azure-sql-serverless',
         component: AzureSqlServerlessComponent,
-        title: 'Azure SQL serverless cost control'
+        title: 'Azure SQL serverless cost control',
       },
       {
         path: 'angular-on-github-pages',
         component: AngularOnGithubPagesComponent,
-        title: 'Angular on Github Pages'
+        title: 'Angular on Github Pages',
       },
     ],
   },
